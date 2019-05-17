c6593b56d6e6be9c8611f926a0bfb12aae1eb594
kutzol63274 <kutzol63274@efc244044.ads1.fh-nuernberg.de>
2019-05-17 19:33:19 +0200

    touch controller; i2c driver (partly)

--- Project/BLE_Examples/BLE_Chat_Master_Slave/inc/HardwareUtil/HW_I2C.h
+++ Project/BLE_Examples/BLE_Chat_Master_Slave/inc/HardwareUtil/HW_I2C.h
@@ -16,10 +16,14 @@
 #include <stdint.h>
 
 volatile uint8_t FLAG_BUSY;
+volatile uint8_t RECV_BUSY;
 
 void hw_i2c_init();
 
 void hw_i2c_write(uint8_t addr, const uint8_t *data, uint8_t length, uint8_t wait, uint8_t end);
+void hw_i2c_read(uint8_t addr, uint8_t registerAddr, uint8_t length);
+
+void hw_i2c_isr_received();
 void hw_i2c_isr_bufferEmpty();
 
 #endif /* BLUEHOME_HARDWAREUTIL_HW_I2C_H_ */
